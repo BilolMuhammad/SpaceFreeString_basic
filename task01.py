5270ab79f76e996dd7d8cfb95416ea5d18b92617
PRESTIGE <mamatqulov@gmail.com>
2023-01-07 09:15:25 +0500

    o1

--- task01.py
+++ task01.py
@@ -1,4 +1,4 @@
-def remove_spaces(string: str)-> str:
+def remove_spaces(string: str) -> str:
     """
     Removes all spaces from a string of fixed length of 5 characters.
     args:
@@ -8,3 +8,23 @@ def remove_spaces(string: str)-> str:
     """
     # Your code here
     pass
+    ans = ''
+    idx = 0
+    if string[idx] != ' ':
+        ans += string[idx]
+    idx += 1
+    if string[idx] != ' ':
+        ans += string[idx]
+    idx += 1
+    if string[idx] != ' ':
+        ans += string[idx]
+    idx += 1
+    if string[idx] != ' ':
+        ans += string[idx]
+    idx += 1
+    if string[idx] != ' ':
+        ans += string[idx]
+    return ans
+
+
+print(remove_spaces('s f s '))
